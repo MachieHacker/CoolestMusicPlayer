92824699a0a325345e4b564a2432f77e12bcc440
MachieHacker <HacunaBoy@protonmail.com>
2021-06-11 01:49:43 +0300

    Done buttons, slider and that music can play

---
+++ CoolestMusicPlayer/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace CoolestMusicPlayer.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayButton.Content == "Play")
+            {
+                PlayButton.Content = "Pause";
+                mediaPlayer.Play();
+            }
+            else
+            {
+                PlayButton.Content = "Play";
+                mediaPlayer.Pause();
+            }
+        }
+
+        private void PrevButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaPlayer.Open(new Uri(openFileDialog.FileName));
+            }
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (mediaPlayer.Source != null)
+            {
+                Timer.Content = String.Format("{0}", mediaPlayer.Position.ToString(@"mm\:ss"));
+                FullTime.Content = String.Format("{0}", mediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+                ProgressSlider.Minimum = 0;
+                ProgressSlider.Maximum = mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                ProgressSlider.Value = mediaPlayer.Position.TotalSeconds;
+            }
+            else
+            {
+                Timer.Content = "No file selected...";
+            }
+        }
+    }
+}
